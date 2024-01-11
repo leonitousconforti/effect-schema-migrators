57ecb207266ac454e75636adf5af5c2ca827a92d
Leo Conforti <leo@leoconforti.us>
2024-01-11 02:32:08 +0000

    save

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-npm install -g @microsoft/rush tsx
-rush install --purge --full
+npm install -g @microsoft/rush pnpm tsx
+rush install --purge
+rush update --full
 rush update-autoinstaller --name rush-prettier
 rush update-autoinstaller --name rush-commitlint
 rush update-autoinstaller --name rush-github-action-cache
